63ecb7a90a2f111f36e598fcc65958ece54d0df4
Tony Bowden <tony@tmtm.com>
2018-04-04 07:29:29 +0100

    Rewrite to less imperative style
    Rubocop doesn't like the arg to `scrape` simply being called `h`. Rather
    than simply renaming that to `config`, this is a good time to also bring
    the scraper in-line with the more up-to-date approach of being less
    imperative, and of making sure we can succeffully gather up all the data
    before cleaning out the database.

--- scraper.rb
+++ scraper.rb
@@ -9,22 +9,24 @@ require_rel 'lib'
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
-def scrape(h)
-  url, klass = h.to_a.first
+def scraper(config)
+  url, klass = config.to_a.first
   klass.new(response: Scraped::Request.new(url: url).response)
 end
 
-def scrape_list(list_url)
-  page = scrape(list_url => MembersPage)
-  page.member_urls.each { |url| scrape_person(url) }
-  scrape_list(page.next_page) unless page.next_page.to_s.empty?
+def members_data(list_url)
+  page = scraper(list_url => MembersPage)
+  data = page.member_urls.map { |url| member_data(url) }
+  return data if page.next_page.empty?
+  data + members_data(page.next_page)
 end
 
-def scrape_person(url)
-  data = scrape(url => MemberPage).to_h
-  puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
-  ScraperWiki.save_sqlite(%i[id term], data)
+def member_data(url)
+  scraper(url => MemberPage).to_h
 end
+
+data = members_data('https://www.pa.org.za/organisation/national-assembly/people/')
+data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
 
 ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-scrape_list('https://www.pa.org.za/organisation/national-assembly/people/')
+ScraperWiki.save_sqlite(%i[id term], data)
